fded7d13ec25eb524eb034977f36483875765fe7
muhammad rahmani <trtpta@gmail.com>
2025-12-18 09:09:26 +0330

    https://www.typescriptlang.org/docs/handbook/intro.html

---
+++ index0.ts
@@ -0,0 +1,21 @@
+// https://www.typescriptlang.org/docs/handbook/intro.html
+
+class Student {
+  fullName: string;
+  constructor(firstName: string, middleInitial: string, lastName: string) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+console.log(user);
